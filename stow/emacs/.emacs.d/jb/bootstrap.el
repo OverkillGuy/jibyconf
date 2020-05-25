3ac052d2a3604e0945afd41353894f28cca74cf6
Jb DOYON <jb@jiby.tech>
2020-05-26 00:08:38 +0100

    emacs: Change emacs bootstrap to litterate config
    Bring back the org mode file for description, tangled into an elisp
    file to use for config purposes.
    
    Now that stow/.stowrc file disable deployment of the org mode source,
    we can now have self contained "package documentation".
    
    After much consideration, the tangled elisp file will be committed too,
    despite being redundant change with the org file. This ensures
    that a cloned git repo can be deployed without having to require emacs
    to "compile" (tangle) "source code" (org mode files) into "binary
    format" (elisp).
    
    Intended layout:
    
    stow/
    ├── emacs
    │   ├── bootstrap.org            <-- Documentation (not stowed)
    │   ├── ...
    │   └── .emacs.d
    │       └── jb
    │           └── bootstrap.el     <-- loads vanilla.el, package.el ...
    └── emacs-vanilla
        ├── vanilla.org              <-- Documentation (not stowed)
        └── .emacs.d
            └── jb
                ├── packages
                │   └── ...
                └── vanilla.el       <-- Loaded by bootstrap.el
    
    Currently only for stow/emacs/ as a first step, but intended for others soon.

--- stow/emacs/.emacs.d/jb/bootstrap.el
+++ stow/emacs/.emacs.d/jb/bootstrap.el
@@ -1,19 +1,15 @@
-; temporary startup-performance settings
-; Reducing startup time by 50%:
-; From https://www.reddit.com/r/emacs/comments/3kqt6e/2_easy_little_known_steps_to_speed_up_emacs_start/
 (setq gc-cons-threshold 999999999)
 (message "gc-cons-threshold temporarily set to %S"
 	 gc-cons-threshold)
 
 (let ((file-name-handler-alist nil))
-  ; dangling paren intentional, everything below should be wrapped
 
   (if (file-exists-p "vanilla.el")
       (load-file "vanilla.el"))
 
   (if (file-exists-p "package-config.el")
       (load-file "package-config.el"))
-  
+
   (defun jb-post-frame-config-load (&optional frame)
     "Make frame- and/or terminal-local changes."
     (with-selected-frame (or frame (selected-frame))
@@ -29,6 +25,5 @@
   (if (file-exists-p "external-pkg-config.el")
       (load-file "external-pkg-config.el"))
 
-					; Revert the startup-performance settings
-  (setq gc-cons-threshold 800000)
-  (message "gc-cons-threshold restored to %S"  gc-cons-threshold))
+(setq gc-cons-threshold 800000)
+(message "gc-cons-threshold restored to %S"  gc-cons-threshold))
