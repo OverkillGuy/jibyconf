e83055285bb52d3f99beadaa2a2f179f564f0436
Jb DOYON <jb@jiby.tech>
2021-05-17 01:35:20 +0100

    guix: Update simple package manifests
    Replace Vagrantfile's provisioning via guix install xyz to use manifests

---
+++ stow/guix/.config/guix/manifests/devtools.scm
@@ -0,0 +1,3 @@
+; A few non-language-specific dev tools
+(specifications->manifest
+ '("emacs" "curl" "jq" "htop" "tree" "the-silver-searcher" "make"))
---
+++ stow/guix/.config/guix/manifests/stow_base.scm
@@ -0,0 +1,3 @@
+; Enough packages to clone jibyconf repo + deploy GNU stow packages
+(specifications->manifest
+ '("git" "stow"))
