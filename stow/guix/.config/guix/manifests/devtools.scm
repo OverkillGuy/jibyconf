8a67e85ff99a57282a8d084e58a0d1cdd39d1166
Jb DOYON <jb@jiby.tech>
2021-05-17 02:45:48 +0100

    guix: Update devtools with language-specific packages

--- stow/guix/.config/guix/manifests/devtools.scm
+++ stow/guix/.config/guix/manifests/devtools.scm
@@ -1,3 +1,9 @@
-; A few generic devtools (not language specific)
+; Dev tools
 (specifications->manifest
- '("emacs" "git" "stow" "make" "curl" "jq" "htop" "tree" "the-silver-searcher"))
+ '("emacs" "git" "stow" "make" "curl" "jq" "htop" "tree" "the-silver-searcher"
+   ;; Python-specific
+   "python" "python-pre-commit" "poetry"
+   ;; Rust
+   "rust" "rust:cargo" "rust:rustfmt"
+   ;; Note the missing target in guix: clippy (rust linter)
+   ;; "rust-clippy"))
