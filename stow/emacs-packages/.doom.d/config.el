fab847a2a2f61cd6274699acdf1c3986e7991c36
Jb DOYON <jb@jiby.tech>
2022-04-20 03:48:48 +0100

    emacs: Add "take-both" to mergetool, bound to d

--- stow/emacs-packages/.doom.d/config.el
+++ stow/emacs-packages/.doom.d/config.el
@@ -245,3 +245,14 @@ will not be modified."
 
 (add-hook 'python-mode-hook #'python-src-fill-black-mode-hook)
 (evil-set-initial-state 'dired-mode 'emacs)
+
+
+;; Add "take-both" option to merge, bound to d
+(defun ediff-copy-both-to-C ()
+  (interactive)
+  (ediff-copy-diff ediff-current-difference nil 'C nil
+                   (concat
+                    (ediff-get-region-contents ediff-current-difference 'A ediff-control-buffer)
+                    (ediff-get-region-contents ediff-current-difference 'B ediff-control-buffer))))
+(defun add-d-to-ediff-mode-map () (define-key ediff-mode-map "d" 'ediff-copy-both-to-C))
+(add-hook 'ediff-keymap-setup-hook 'add-d-to-ediff-mode-map)
