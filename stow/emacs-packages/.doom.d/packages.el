30fbeba679bd42990728e743e7c061f808b439c9
Jb DOYON <jb@jiby.tech>
2025-12-10 17:58:53 +0000

    emacs: Add typst mode
    Relies on treesitter, which new Emacs bakes in

--- stow/emacs-packages/.doom.d/packages.el
+++ stow/emacs-packages/.doom.d/packages.el
@@ -37,6 +37,9 @@
 
 
 (package! rainbow-mode)
+(package! typst-ts-mode
+  :recipe
+  (:host codeberg :repo "meow_king/typst-ts-mode"))
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/raxod502/straight.el#the-recipe-format
